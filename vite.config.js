4a7699795b989697070dec71c1550a029bc6e9d3
anac2828 <anac2828@yahoo.com>
2025-09-05 19:35:12 -0700

    modified .json and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: { outDir: 'dist' },
   plugins: [htmlPurge()],
-  base: 'https://anac2828.github.io/chef/',
+  base: '/chef',
 });
