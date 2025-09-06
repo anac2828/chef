6a0dba87ee7a66d66374a3fbc5e812351a107264
anac2828 <anac2828@yahoo.com>
2025-09-05 18:56:57 -0700

    renamed build folder to dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import htmlPurge from 'vite-plugin-purgecss';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: { outDir: 'build' },
+  build: { outDir: 'dist' },
   plugins: [htmlPurge()],
   base: 'https://anac2828.github.io/chef/',
 });
